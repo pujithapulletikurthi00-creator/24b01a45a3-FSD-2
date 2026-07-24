11826bd2f8a513734ff758680401ac67d539c2ec
pujitha <pujithapulletikurthi00@gmail.com>
2026-07-24 22:00:33 +0530

    Create classwithstatic.ts

---
+++ week-4/classwithstatic.ts
@@ -0,0 +1,10 @@
+class College {
+
+    static collegeName: string = "SVECW";
+
+    static displayCollege(): void {
+        console.log("College Name: " + College.collegeName);
+    }
+}
+
+College.displayCollege();
