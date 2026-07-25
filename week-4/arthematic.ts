7b9c10cfc4b2c4411acd73641f33bc887d6451b3
pujitha <pujithapulletikurthi00@gmail.com>
2026-07-25 08:39:03 +0530

    Update arthematic.ts

--- week-4/arthematic.ts
+++ week-4/arthematic.ts
@@ -13,3 +13,9 @@ export function multiply(a: number, b: number): number {
 export function divide(a: number, b: number): number {
     return a / b;
 }
+
+// Function calls
+console.log("Addition:", add(10, 5));
+console.log("Subtraction:", subtract(10, 5));
+console.log("Multiplication:", multiply(10, 5));
+console.log("Division:", divide(10, 5));
